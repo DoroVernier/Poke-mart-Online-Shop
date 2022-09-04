179b7ea0abdfa658302b066c3102a87333944302
DoroVernier <doro.vernier@outlook.de>
2022-09-04 20:46:19 +0200

    add routing functionality and styling

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,19 +1,42 @@
+import styled from "styled-components";
+
 export default function ShoppingCart({
   cartItem,
-  setBoughtItems,
+  onSetBoughtItems,
   boughtItems,
 }) {
-  function handleRemove(boughtItems, id) {
-    const newList = boughtItems.filter((boughtItem) => boughtItem.id !== id);
-    setBoughtItems(newList);
+  function handleRemove(itemId) {
+    const newList = boughtItems.filter(
+      (boughtItem) => boughtItem.id !== itemId
+    );
+    onSetBoughtItems(newList);
   }
 
   return (
-    <li>
+    <Cards>
       <img alt="" src={cartItem.image} />
       <p>{cartItem.cost} $</p>
       {cartItem.name}
-      <button onClick={handleRemove}>remove item</button>
-    </li>
+      <RemButton onClick={handleRemove}>X</RemButton>
+    </Cards>
   );
 }
+ 
+const RemButton = styled.button`
+padding: 5px; 
+background-color: rgb(251 27 27) ; 
+border-radius: 50%; 
+margin: 10px; 
+border: 2px solid black; 
+color: white; 
+`;
+const Cards = styled.li`
+display: grid; 
+grid-template-column: 1fr;
+list-style: none;
+  background-color: white;
+  border-radius: 20px;
+  padding: 15px;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+`;
+
--- src/components/ShoppingItem.js
+++ src/components/ShoppingItem.js
@@ -1,45 +1,62 @@
 import { nanoid } from "nanoid";
 import { useEffect, useState } from "react";
+import styled from "styled-components";
 
 export default function ShoppingItem({ article, onAddItem }) {
-    const [articleDetails, setArticleDetails] = useState("");
+  const [articleDetails, setArticleDetails] = useState("");
 
-    const furtherInfo = article.url;
+  const furtherInfo = article.url;
 
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const response = await fetch(furtherInfo);
-                const data = await response.json();
-                setArticleDetails({
-                    id: nanoid(),
-                    image: data.sprites.default,
-                    cost: data.cost,
-                    name: data.name,
-                });
-            } catch (error) {
-                console.error(error);
-            }
-        }
-        fetchData();
-    }, [furtherInfo]);
-
-    function handleClick(id) {
-        const newItem = {
-            id: nanoid(),
-            image: articleDetails.image,
-            cost: articleDetails.cost,
-            name: article.name,
-        };
-        onAddItem(newItem);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(furtherInfo);
+        const data = await response.json();
+        setArticleDetails({
+          id: nanoid(),
+          image: data.sprites.default,
+          cost: data.cost,
+          name: data.name,
+        });
+      } catch (error) {
+        console.error(error);
+      }
     }
+    fetchData();
+  }, [furtherInfo]);
 
-    return (
-        <li>
-            <img alt="" src={articleDetails.image} />
-            <p>{articleDetails.cost} $</p>
-            {article.name}
-            <button onClick={handleClick}>add item</button>
-        </li>
-    );
+  function handleClick(id) {
+    const newItem = {
+      id: nanoid(),
+      image: articleDetails.image,
+      cost: articleDetails.cost,
+      name: article.name,
+    };
+    onAddItem(newItem);
+  }
+
+  return (
+    <Cards>
+      <img alt="" src={articleDetails.image} />
+      <p>{articleDetails.cost} $</p>
+      {article.name}
+      <AddButton onClick={handleClick}>+</AddButton>
+    </Cards>
+  );
 }
+
+const Cards = styled.li`
+  background-color: white;
+  border-radius: 20px;
+  padding: 15px;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+`;
+const AddButton = styled.button`
+padding: 5px; 
+background-color: rgb(0 117 190)  ; 
+border-radius: 50%; 
+margin: 10px; 
+border: 2px solid black; 
+color: white; 
+`
+
